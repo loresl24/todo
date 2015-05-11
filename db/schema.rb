33c121ffe0309805a98ab499e69584be78372ad9
Lorena <tecnologia@iMac-de-Admin-147.local>
2015-05-11 10:01:20 +0200

    contacto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427073701) do
+ActiveRecord::Schema.define(version: 20150511072938) do
 
   create_table "pets", force: true do |t|
     t.string   "name"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20150427073701) do
   create_table "tasks", force: true do |t|
     t.string   "description"
     t.boolean  "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
